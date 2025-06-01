76b3da58d825e2974d242cc332005fec3e2570c9
Charles Mariuzza <charles@mariuzza.de>
2025-06-01 14:44:58 +0200

    Initialize Project #1
    - Adds new project in Xcode
    - Adds project settings
    - Adds App Logo + Display Name

---
+++ SF Companion/SF Companion/SF_CompanionApp.swift
@@ -0,0 +1,32 @@
+//
+//  SF_CompanionApp.swift
+//  SF Companion
+//
+//  Created by Charles Mariuzza on 01.06.25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SF_CompanionApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ SF Companion/SF CompanionTests/SF_CompanionTests.swift
@@ -0,0 +1,16 @@
+//
+//  SF_CompanionTests.swift
+//  SF CompanionTests
+//
+//  Created by Charles Mariuzza on 01.06.25.
+//
+
+import Testing
+
+struct SF_CompanionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
